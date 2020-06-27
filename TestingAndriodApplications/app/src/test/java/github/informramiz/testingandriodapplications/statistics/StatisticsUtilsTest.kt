1dd356a35634734db1a1f9b0e6623e7767577f90
Ramiz Raja <informramiz@gmail.com>
2020-06-27 05:02:57 +0500

    test: Write different input tests

--- TestingAndriodApplications/app/src/test/java/github/informramiz/testingandriodapplications/statistics/StatisticsUtilsTest.kt
+++ TestingAndriodApplications/app/src/test/java/github/informramiz/testingandriodapplications/statistics/StatisticsUtilsTest.kt
@@ -13,7 +13,7 @@ class StatisticsUtilsTest {
     //if there is one active task and no completed task then there are 100%
     //active tasks and 0% completed tasks
     @Test
-    fun getActiveAndCompletedStats_noCompleted_returnsZeroHundred() {
+    fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
         //GIVEN: list of tasks containing only one active task
         val tasks = listOf(Task("title", "description", isCompleted = false))
 
@@ -23,5 +23,34 @@ class StatisticsUtilsTest {
         //THEN: there should be 100% active and 0% completed tasks
         assertThat(result.activeTasksPercent, `is`(100f))
         assertThat(result.completedTasksPercent, `is`(0f))
+    }
+
+    @Test
+    fun getActiveAndCompleteStats_noActive_returnsZeroHundred() {
+        //GIVEN: list of tasks containing only one complete task
+        val tasks = listOf(Task("title", "description", isCompleted = true))
+
+        //WHEN: you call getActiveAndCompleteStats
+        val result = getActiveAndCompletedStats(tasks)
+
+        //THEN: there should be 0% active and 100% completed tasks
+        assertThat(result.activeTasksPercent, `is`(0f))
+        assertThat(result.completedTasksPercent, `is`(100f))
+    }
+
+    @Test
+    fun getActiveAndCompleteStats_both_returnsFiftyFifty() {
+        //GIVEN: list of tasks containing only one complete task
+        val tasks = listOf(
+            Task("title", "description", isCompleted = true),
+            Task("title", "description", isCompleted = false)
+        )
+
+        //WHEN: you call getActiveAndCompleteStats
+        val result = getActiveAndCompletedStats(tasks)
+
+        //THEN: there should be 0% active and 100% completed tasks
+        assertThat(result.activeTasksPercent, `is`(50f))
+        assertThat(result.completedTasksPercent, `is`(50f))
     }
 }
