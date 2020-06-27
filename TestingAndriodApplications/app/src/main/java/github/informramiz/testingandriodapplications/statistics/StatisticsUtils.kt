69e2a270e513a66e47f99142d509239bd94170b7
Ramiz Raja <informramiz@gmail.com>
2020-06-27 05:05:55 +0500

    fix: Null or empty returns wrong output

--- TestingAndriodApplications/app/src/main/java/github/informramiz/testingandriodapplications/statistics/StatisticsUtils.kt
+++ TestingAndriodApplications/app/src/main/java/github/informramiz/testingandriodapplications/statistics/StatisticsUtils.kt
@@ -6,7 +6,11 @@ import github.informramiz.testingandriodapplications.data.Task
  * Function that does some trivial computation. Used to showcase unit tests.
  */
 internal fun getActiveAndCompletedStats(tasks: List<Task>?): StatsResult {
-    val totalTasks = tasks!!.size
+    if (tasks.isNullOrEmpty()) {
+        return StatsResult(0f, 0f)
+    }
+
+    val totalTasks = tasks.size
     val numberOfActiveTasks = tasks.count { it.isActive }
     return StatsResult(
         activeTasksPercent = 100f * numberOfActiveTasks / tasks.size,
