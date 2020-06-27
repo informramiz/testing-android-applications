14f5ce4f42886da4cd9f303570774d6a77515a01
Ramiz Raja <informramiz@gmail.com>
2020-06-27 06:40:02 +0500

    test: Test live data

---
+++ TestingAndriodApplications/app/src/test/java/github/informramiz/testingandriodapplications/tasks/TasksViewModelTest.kt
@@ -0,0 +1,35 @@
+package github.informramiz.testingandriodapplications.tasks
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import github.informramiz.testingandriodapplications.util.getOrAwaitValue
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.CoreMatchers.nullValue
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Created by Ramiz Raja on 27/06/2020.
+ */
+@RunWith(AndroidJUnit4::class)
+class TasksViewModelTest {
+    //let's make sure all background operations run on the same thread to keep them
+    //in order to avoid any undesired behavior
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+    @Test
+    fun addNewTask_call_setsNewTaskEvent() {
+        //GIVEN:
+        val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+
+        //WHEN:
+        tasksViewModel.addNewTask()
+
+        //THEN:
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
+        assertThat(value.getContentIfNotHandled(), not(nullValue()))
+    }
+}
