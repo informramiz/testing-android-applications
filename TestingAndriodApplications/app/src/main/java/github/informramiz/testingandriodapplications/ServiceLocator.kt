7eb85b961dde08d325e348a9a7ad4d7a8f4c6e6a
Ramiz Raja <informramiz@gmail.com>
2020-07-01 18:06:55 +0500

    test: Write method reset repository

--- TestingAndriodApplications/app/src/main/java/github/informramiz/testingandriodapplications/ServiceLocator.kt
+++ TestingAndriodApplications/app/src/main/java/github/informramiz/testingandriodapplications/ServiceLocator.kt
@@ -1,23 +1,26 @@
 package github.informramiz.testingandriodapplications
 
 import android.content.Context
+import androidx.annotation.VisibleForTesting
 import androidx.room.Room
-import androidx.room.RoomDatabase
 import github.informramiz.testingandriodapplications.data.source.DefaultTasksRepository
 import github.informramiz.testingandriodapplications.data.source.TasksRepository
 import github.informramiz.testingandriodapplications.data.source.local.TasksLocalDataSource
 import github.informramiz.testingandriodapplications.data.source.local.ToDoDatabase
 import github.informramiz.testingandriodapplications.data.source.remote.TasksRemoteDataSource
+import kotlinx.coroutines.runBlocking
 
 
 /**
  * Created by Ramiz Raja on 01/07/2020.
  */
 object ServiceLocator {
+    private val lock = Any()
     private var database: ToDoDatabase? = null
 
     @Volatile
     var tasksRepository: TasksRepository? = null
+        @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE) set
 
     fun provideTasksRepository(context: Context): TasksRepository {
         synchronized(this) {
@@ -44,5 +47,18 @@ object ServiceLocator {
         ).build()
         database = result
         return result
+    }
+
+    @VisibleForTesting
+    fun resetRepository() {
+        synchronized(lock) {
+            runBlocking { TasksRemoteDataSource.deleteAllTasks() }
+        }
+        database?.apply {
+            clearAllTables()
+            close()
+        }
+        database = null
+        tasksRepository = null
     }
 }
