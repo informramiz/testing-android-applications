9aa5f2334784b305f1c14886e892ca8d261596d2
Ramiz Raja <informramiz@gmail.com>
2020-07-03 16:14:18 +0500

    test: Completing test updates data and snackbar message

--- TestingAndriodApplications/app/src/test/java/github/informramiz/testingandriodapplications/tasks/TasksViewModelTest.kt
+++ TestingAndriodApplications/app/src/test/java/github/informramiz/testingandriodapplications/tasks/TasksViewModelTest.kt
@@ -2,10 +2,17 @@ package github.informramiz.testingandriodapplications.tasks
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import github.informramiz.testingandriodapplications.R
 import github.informramiz.testingandriodapplications.data.Task
 import github.informramiz.testingandriodapplications.data.source.FakeTasksRepository
 import github.informramiz.testingandriodapplications.util.getOrAwaitValue
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
 import org.hamcrest.CoreMatchers.*
+import org.junit.After
 import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Rule
@@ -16,11 +23,14 @@ import org.junit.runner.RunWith
  * Created by Ramiz Raja on 27/06/2020.
  */
 @RunWith(AndroidJUnit4::class)
+@ExperimentalCoroutinesApi
 class TasksViewModelTest {
     //let's make sure all background operations run on the same thread to keep them
     //in order to avoid any undesired behavior
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
+
+    private val testCoroutineDispatcher = TestCoroutineDispatcher()
 
     //dependencies
     private lateinit var fakeTasksRepository: FakeTasksRepository
@@ -31,6 +41,10 @@ class TasksViewModelTest {
     //make sure each test has a fresh instance of subject under test
     @Before
     fun setup() {
+        //Dispatcher.Main uses Android's Looper.Main but Android's Main looper is not available
+        // in local tests so we have to change Dispatcher.Main with our test dispatcher
+        Dispatchers.setMain(testCoroutineDispatcher)
+
         fakeTasksRepository = FakeTasksRepository()
         val task1 = Task("Title1", "Description1")
         val task2 = Task("Title2", "Description2", true)
@@ -38,6 +52,12 @@ class TasksViewModelTest {
         fakeTasksRepository.addTasks(task1, task2, task3)
 
         tasksViewModel = TasksViewModel(fakeTasksRepository)
+    }
+
+    @After
+    fun teardown() {
+        Dispatchers.resetMain()
+        testCoroutineDispatcher.cleanupTestCoroutines()
     }
 
     @Test
@@ -59,4 +79,19 @@ class TasksViewModelTest {
         val value = tasksViewModel.tasksAddViewVisible.getOrAwaitValue()
         assertThat(value, `is`(true))
     }
+
+    @Test
+    fun completeTask_dataAndSnackbarMessageUpdated() {
+        //WHEN: A tasks is marked as complete through TasksViewModel
+        val task = Task("Title", "Dispatcher")
+        fakeTasksRepository.addTasks(task)
+        tasksViewModel.completeTask(task, true)
+
+        //THEN:
+        // 2. snackbar message live data should be updated
+        assertThat(fakeTasksRepository.tasksServiceData[task.id]?.isCompleted, `is`(true))
+        // 1. Data in repository
+        assertThat(tasksViewModel.snackbarText.getOrAwaitValue().getContentIfNotHandled(), `is`(R.string.task_marked_complete))
+    }
+
 }
